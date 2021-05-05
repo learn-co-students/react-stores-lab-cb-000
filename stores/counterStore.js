3bcbd2dd80b3936b2ac5545b33a9bb8cf136dcc7
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-05 20:15:31 +0000

    Done.

--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,19 @@
-class CounterStore {
-  // Your implementation here.
-  // Hint: extend the Store class!
+import React from 'react';
+import Store from './Store';
+
+class CounterStore extends Store{
+  constructor(props){
+    super(props);
+    this.state = 0;
+  }
+  increment(){
+    this.setState(this.getState() + 1);
+  }
+
+  decrement(){
+    this.setState(this.getState() - 1);
+  }
+
 }
 
 const counterStore = new CounterStore();
